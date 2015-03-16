f2cecc2b987821aaa14f655376ff40e1058a3803
Eric Liao <rcliao01@gmail.com>
2015-03-16 09:58:59 -0700

    chore(config): update example configuration file

--- example-config.js
+++ example-config.js
@@ -1,8 +1,17 @@
 // A simple configuration for the system
 var config = {
 	// see your Github at https://github.com/settings/applications
-	GITHUB_CLIENT_ID: 'Your Github Client ID',
-	GITHUB_CLIENT_SECRET: 'Your Github Client Secret'
+	github: {
+		GITHUB_CLIENT_ID: 'Your Github Client ID',
+		GITHUB_CLIENT_SECRET: 'Your Github Client Secret'
+	},
+	database: {
+		'host': 'localhost',
+		'user': 'root',
+		'password': '',
+		'database': 'hacky_wiki',
+		'users_table': 'users'
+	}
 };
 
 module.exports = config;
